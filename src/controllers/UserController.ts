2ad70eb0c433724de2e81e40002729e64b14e47f
Abhishek SAINI <saini.abhishek@hotmail.com>
2023-09-25 20:50:16 +0200

    feat: improve authorization

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,6 +4,8 @@ import { BadRequestError } from '../middleware/ApiError';
 import bcrypt from 'bcrypt';
 import { UserModel } from '../models/UserModel';
 import { SuccessResponse } from '../middleware/ApiResponse';
+import AuthHelper from '../helpers/AuthHelper';
+import { Token } from 'app-request';
 
 class UserController {
   register = asyncHandler(async (req, res) => {
@@ -24,14 +26,11 @@ class UserController {
     };
 
     const newUser = await UserModel.create(userObj);
+    const tokens: Token = AuthHelper.createTokens(newUser);
 
     new SuccessResponse('User registered successfully', {
-      user: {
-        _id: newUser._id,
-        email: newUser.email,
-        firstname: newUser.firstname,
-        lastname: newUser.lastname,
-      },
+      token: tokens.accessToken,
+      user: UserHelper.sanitizedUser(newUser),
     }).send(res);
   });
 }
--- src/helpers/UserHelper.ts
+++ src/helpers/UserHelper.ts
@@ -5,12 +5,30 @@ const fullName = (firstname?: string, lastname?: string) => {
   return `${firstname || ''}${lastname ? ' ' + lastname : ''}`;
 };
 
-const findById = async (id: Types.ObjectId): Promise<User | null> => {
-  return UserModel.findOne({ _id: id }).select('-password').exec();
+const sanitizedUser = (user: User): User => {
+  return {
+    _id: user._id,
+    email: user.email,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    name: fullName(user.firstname, user.lastname),
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  };
 };
 
-const findByEmail = async (email: string): Promise<User | null> => {
-  return UserModel.findOne({ email: email }).exec();
+const findById = async (
+  id: Types.ObjectId,
+  selectFields = ''
+): Promise<User | null> => {
+  return UserModel.findOne({ _id: id }).select(selectFields).exec();
 };
 
-export default { findByEmail, fullName, findById };
+const findByEmail = async (
+  email: string,
+  selectFields = ''
+): Promise<User | null> => {
+  return UserModel.findOne({ email: email }).select(selectFields).exec();
+};
+
+export default { findByEmail, fullName, findById, sanitizedUser };
