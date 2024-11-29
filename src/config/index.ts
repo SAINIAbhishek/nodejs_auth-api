104efede1727184593066a25397fb4369ffb037f
Abhishek SAINI <saini.abhishek@hotmail.com>
2024-11-29 12:04:32 +0100

    config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,64 +1,76 @@
-export const ENVIRONMENT = process.env.NODE_ENV;
-export const PORT = process.env.PORT;
-export const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3001';
-export const CORS_URL = process.env.CORS_URL?.split(',') || [];
-export const API_VERSION = process.env.API_VERSION;
-export const FRONTEND_RESET_URL = process.env.FRONTEND_RESET_URL;
-export const MAILTRAP_EMAIL_ENV = process.env.MAILTRAP_EMAIL_ENV || 'testing';
+import { Config } from '../types/config-type';
 
-export const DATE_FORMAT = process.env.DATE_FORMAT || 'yyyy-MM-dd';
-export const DATE_FULL_FORMAT = process.env.DATE_FULL_FORMAT || 'yyyy-MM-dd HH:mm:ss.SSS';
-
-export const MAILTRAP_EMAIL = {
-  testing: {
-    username: process.env.MAILTRAP_TESTING_USERNAME || '',
-    password: process.env.MAILTRAP_TESTING_PASSWORD || '',
-    host: process.env.MAILTRAP_TESTING_HOST || '',
-    port: parseInt(process.env.MAILTRAP_TESTING_PORT || ''),
+const config: Config = {
+  server: {
+    environment: process.env.NODE_ENV || 'development',
+    port: parseInt(process.env.PORT || '3001'),
+    url: process.env.SERVER_URL || 'http://localhost:3001',
+    apiVersion: process.env.API_VERSION || 'v1',
+    emailEnv: process.env.MAILTRAP_EMAIL_ENV || 'testing',
   },
-  prod: {
-    username: process.env.MAILTRAP_USERNAME || '',
-    password: process.env.MAILTRAP_PASSWORD || '',
-    host: process.env.MAILTRAP_HOST || '',
-    port: parseInt(process.env.MAILTRAP_PORT || ''),
+  cors: {
+    allowedOrigins: process.env.CORS_URL?.split(',') || ['http://localhost:3000'],
+  },
+  frontend: {
+    resetUrl: process.env.FRONTEND_RESET_URL || 'http://localhost:3000/',
+  },
+  date: {
+    format: process.env.DATE_FORMAT || 'yyyy-MM-dd',
+    fullFormat: process.env.DATE_FULL_FORMAT || 'yyyy-MM-dd HH:mm:ss.SSS',
+  },
+  mailtrap: {
+    testing: {
+      username: process.env.MAILTRAP_TESTING_USERNAME || '',
+      password: process.env.MAILTRAP_TESTING_PASSWORD || '',
+      host: process.env.MAILTRAP_TESTING_HOST || '',
+      port: parseInt(process.env.MAILTRAP_TESTING_PORT || ''),
+    },
+    prod: {
+      username: process.env.MAILTRAP_USERNAME || '',
+      password: process.env.MAILTRAP_PASSWORD || '',
+      host: process.env.MAILTRAP_HOST || '',
+      port: parseInt(process.env.MAILTRAP_PORT || ''),
+    },
+  },
+  limiter: {
+    loginWS: parseInt(process.env.LIMITER_LOGIN_WS || '120000'),
+    ipWS: parseInt(process.env.LIMITER_IP_WS || '900000'),
+    forgotPasswordWS: parseInt(process.env.LIMITER_FORGOT_PASSWORD_WS || '120000'),
+    loginMaxAttempt: parseInt(process.env.LIMITER_LOGIN_ATTEMPT || '5'),
+    ipMaxAttempt: parseInt(process.env.LIMITER_IP_ATTEMPT || '100'),
+    forgotPasswordMaxAttempt: parseInt(process.env.LIMITER_FORGOT_PASSWORD_ATTEMPT || '2'),
+  },
+  cookie: {
+    login: process.env.COOKIE_LOGIN || 'rememberme',
+    maxAge: parseInt(process.env.COOKIE_MAX_AGE_SEC || '0'),
+  },
+  db: {
+    uri: process.env.DB_URI || 'mongodb://localhost:27017/',
+    name: process.env.DB_DATABASE_NAME || 'node-db-v1',
+    host: process.env.DB_HOST || '127.0.0.1',
+    username: process.env.DB_USERNAME || 'nodeApplicationUser',
+    password: process.env.DB_PWD || '123456789',
+    port: parseInt(process.env.DB_PORT || '27017'),
+    minPoolSize: parseInt(process.env.DB_MIN_POOL_SIZE || '5'),
+    maxPoolSize: parseInt(process.env.DB_MAX_POOL_SIZE || '10'),
+    connectTimeoutMS: parseInt(process.env.DB_CONNECT_TIMEOUT_MS || '60000'),
+    socketTimeoutMS: parseInt(process.env.DB_SOCKET_TIMEOUT_MS || '45000'),
+  },
+  token: {
+    accessTokenValidity: parseInt(process.env.ACCESS_TOKEN_VALIDITY_SEC || '0'),
+    refreshTokenValidity: parseInt(process.env.REFRESH_TOKEN_VALIDITY_SEC || '0'),
+    passwordResetTokenValidity: parseInt(process.env.PASSWORD_RESET_TOKEN_VALIDITY_SEC || '0'),
+    issuer: process.env.TOKEN_ISSUER || 'api.dev.saini.com',
+    audience: process.env.TOKEN_AUDIENCE || 'dev.saini.com',
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET_KEY || '',
+    refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET_KEY || '',
+  },
+  logging: {
+    directory: process.env.LOG_DIR || 'logs',
+    datePattern: process.env.LOG_DATE_PATTERN || 'YYYY-MM-DD-HH',
+    maxSize: process.env.LOG_MAX_SIZE || '20m',
+    maxFiles: process.env.LOG_MAX_FILES || '14d',
   },
 };
 
-export const LIMITER = {
-  loginWS: parseInt(process.env.LIMITER_LOGIN_WS || '120000'),
-  ipWS: parseInt(process.env.LIMITER_IP_WS || '900000'),
-  forgotPasswordWS: parseInt(process.env.LIMITER_FORGOT_PASSWORD_WS || '120000'),
-  loginMaxAttempt: parseInt(process.env.LIMITER_LOGIN_ATTEMPT || '5'),
-  ipMaxAttempt: parseInt(process.env.LIMITER_IP_ATTEMPT || '100'),
-  forgotPasswordMaxAttempt: parseInt(process.env.LIMITER_FORGOT_PASSWORD_ATTEMPT || '2'),
-};
-
-export const COOKIE = {
-  login: process.env.COOKIE_LOGIN || '',
-  maxAge: parseInt(process.env.COOKIE_MAX_AGE_SEC || '0'),
-};
-
-export const DB = {
-  uri: process.env.DB_URI,
-  name: process.env.DB_DATABASE_NAME || '',
-  host: process.env.DB_HOST || '',
-  username: process.env.DB_USERNAME || '',
-  pwd: process.env.DB_PWD || '',
-  port: process.env.DB_PORT || '',
-  minPoolSize: parseInt(process.env.DB_MIN_POOL_SIZE || '5'),
-  maxPoolSize: parseInt(process.env.DB_MAX_POOL_SIZE || '10'),
-  connectTimeoutMS: parseInt(process.env.DB_CONNECT_TIMEOUT_MS || '60000'),
-  socketTimeoutMS: parseInt(process.env.DB_SOCKET_TIMEOUT_MS || '45000'),
-};
-
-export const TOKEN_INFO = {
-  accessTokenValidity: parseInt(process.env.ACCESS_TOKEN_VALIDITY_SEC || '0'),
-  refreshTokenValidity: parseInt(process.env.REFRESH_TOKEN_VALIDITY_SEC || '0'),
-  passwordResetTokenValidity: parseInt(process.env.PASSWORD_RESET_TOKEN_VALIDITY_SEC || '0'),
-  issuer: process.env.TOKEN_ISSUER || '',
-  audience: process.env.TOKEN_AUDIENCE || '',
-  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET_KEY || '',
-  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET_KEY || '',
-};
-
-export const LOG_DIRECTORY = process.env.LOG_DIR;
+export default config;
---
+++ src/types/config-type.ts
@@ -0,0 +1,79 @@
+type mailtrap = {
+  username: string;
+  password: string;
+  host: string;
+  port: number;
+};
+
+type server = {
+  environment: string;
+  port: number;
+  url: string;
+  apiVersion: string;
+  emailEnv: string;
+};
+
+type limiter = {
+  loginWS: number;
+  ipWS: number;
+  forgotPasswordWS: number;
+  loginMaxAttempt: number;
+  ipMaxAttempt: number;
+  forgotPasswordMaxAttempt: number;
+};
+
+type db = {
+  uri: string;
+  name: string;
+  host: string;
+  username: string;
+  port: number;
+  password: string;
+  minPoolSize: number;
+  maxPoolSize: number;
+  connectTimeoutMS: number;
+  socketTimeoutMS: number;
+};
+
+type token = {
+  accessTokenValidity: number;
+  refreshTokenValidity: number;
+  passwordResetTokenValidity: number;
+  issuer: string;
+  audience: string;
+  accessTokenSecret: string;
+  refreshTokenSecret: string;
+};
+
+type logging = {
+  directory: string;
+  maxSize: string;
+  maxFiles: string;
+  datePattern: string;
+};
+
+export type Config = {
+  server: server;
+  cors: {
+    allowedOrigins: string[];
+  };
+  frontend: {
+    resetUrl: string;
+  };
+  mailtrap: {
+    testing: mailtrap;
+    prod: mailtrap;
+  };
+  date: {
+    format: string;
+    fullFormat: string;
+  };
+  limiter: limiter;
+  cookie: {
+    login: string;
+    maxAge: number;
+  };
+  db: db;
+  token: token;
+  logging: logging;
+};
